35e1c5b5a4721fa39d9f98f044c3f731456072fe
jhoisz <jvrda@ufpi.edu.br>
2024-07-15 10:21:21 -0300

    update: project update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2362539359"
+    serviceWorkerVersion: "3891208519"
   }
 });
